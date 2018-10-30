1b10330efba71cfae8b5a74483275fb75719fb80
alex-bowen <alexbowen2017@gmail.com>
2018-10-30 18:40:23 -0500

    Removed classnames

--- routes/routes.js
+++ routes/routes.js
@@ -36,7 +36,7 @@ module.exports = function (app) {
             // load response to cheerio
             var $ = cheerio.load(response.data);
 
-            $("section.fg").each(function (i, element) {
+            $("section").each(function (i, element) {
 
                 var result = {};
 
